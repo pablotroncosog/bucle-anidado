050fad488767281ec7bdae47f3bc5ac1b7ec1748
pablotroncosog <pablo.troncoso@mail.udp.cl>
2022-08-25 22:14:17 +0000

    commit1

---
+++ index.js
@@ -0,0 +1,12 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon']; 
+
+for (let i = 0; i < pronoun.length; i++) {
+    for (let ad = 0; ad < adj.length; ad++) { 
+        for (let ac = 0; ac < noun.length; ac++) 
+        { console.log(pronoun [i] + '' + adj [ad] + '' + noun [ac] + '.com')
+  }        
+ }
+}
+
